047feb6efff7a29366e42f7073b1f2df97c70475
Niall Tiernan <n.tiernan@youwe.nl>
2019-05-05 16:34:31 +0200

    Initial commit

---
+++ Controller/CustomerDelete/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Niall\CustomerDeleteMyAccount\Controller\CustomerDelete;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
